380f9d62f3f1aaf49f871acaf90dc140c49bf11d
Diogo Torres <diogo.torres93@gmail.com>
2019-07-14 21:38:53 -0300

    Obtendo um usuario por ID

--- src/main/java/com/diogotorres/springmongo/services/UserService.java
+++ src/main/java/com/diogotorres/springmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.diogotorres.springmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.diogotorres.springmongo.domain.User;
 import com.diogotorres.springmongo.repository.UserRepository;
+import com.diogotorres.springmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);  
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
